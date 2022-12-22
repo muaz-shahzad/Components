347eb423b92800a0a4aff3c319db613654054e88
M Muaz Shahzad <muazshahzad667@gmail.com>
2022-12-22 21:01:37 +0500

    Add files via upload

---
+++ OTP Generator/script.js
@@ -0,0 +1,11 @@
+function generateOTP()
+{
+    var digits = '0123456789';
+    let OTP = '';
+    for(let i=0;i<4;i++){
+        OTP += digits[Math.floor(Math.random()*10)]
+    }
+    return OTP;
+}
+document.write("OTP of 4 digits ");
+document.write(generateOTP());
